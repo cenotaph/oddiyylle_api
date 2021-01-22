29d57e7805d085aa65dffd9e33ebb8e8a87c990b
John W. Fail <john@pixelache.ac>
2021-01-22 15:11:37 +0200

    secret key base

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module Ysaatio
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.hosts << "localhost"
-    config.secret_key_base = Rails.application.credentials.dig(:secret_key_base)
+    
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
